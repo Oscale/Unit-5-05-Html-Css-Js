83a91b7889135008c07ecdc0407c2b7731bd3797
Oscale <>
2021-05-04 19:34:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('cal').addEventListener ('click', Calbutton)
+
+let width = 0
+
+let lenght = 0
+
+function Calbutton () {
+  width = document.getElementById('widthn').value
+  lenght = document.getElementById('lenghtn').value 
+
+  width = parseFloat(width)
+  lenght = parseFloat(lenght)
+
+  alert (width * lenght)
+}
+
+
